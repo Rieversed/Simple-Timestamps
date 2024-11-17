a28fc00e6c390efc8981497053e67fd92a52062e
Rieversed <LunarScytheYT@gmail.com>
2024-11-17 19:43:38 +0100

    Initial commit

---
+++ src/main/java/com/velocity/simpletimestamps/SimpleTimestampsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.velocity.simpletimestamps;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SimpleTimestampsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
